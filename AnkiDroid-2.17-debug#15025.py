35ed3c04984de484c2f98dbe41fbf14ccd375554
pikaqiuswd <yimeng5418@163.com>
2023-12-22 23:45:11 +0800

    Initial commit

---
+++ AnkiDroid-2.17-debug#15025.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+#
+import uiautomator2 as u2
+
+d = u2.connect()
+
+d.long_click(x, y)
+d.press("power")
+d.long_click(0.556, 0.352)
+d.click(0.404, 0.644)
+d.click(0.521, 0.295)
+d.click(0.265, 0.389)
+d.long_click(0.265, 0.39)
+d.double_click(0.163, 0.387)
+d.click(0.648, 0.663)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
